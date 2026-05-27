362d5009371fe438e46c70adf8cb7fc159b2d655
alpsten <hello.alpsten@gmail.com>
2026-05-27 09:45:36 +0200

    fix: add missing basename to BrowserRouter for GitHub Pages subpat

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./styles/global.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/1001-songs-for-majken">
       <App />
     </BrowserRouter>
   </StrictMode>
